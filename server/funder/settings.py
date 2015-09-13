efa391e26c45d97ca728fe39ffec19ad05ec8b90
Ray Chow <raychow.info@gmail.com>
2015-09-13 21:36:03 +0800

    Modify language and timezone in Django

--- server/funder/settings.py
+++ server/funder/settings.py
@@ -69,9 +69,9 @@ WSGI_APPLICATION = 'funder.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Hong_Kong'
 
 USE_I18N = True
 
